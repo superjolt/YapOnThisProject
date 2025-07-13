c8615b9e08cda67dd26c4a821beaaac94046304b
goo goo ga ga <115384734+superjolt@users.noreply.github.com>
2025-07-13 08:30:35 +0530

    more logging + errors

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import random
 
 load_dotenv()
 
-SESSION_ID = os.getenv("SESSION_ID") or (print("SESSION_ID is MISSING!!")) or exit(1)
+SESSION_ID = os.getenv("SESSION_ID") or (print("SESSION_ID is MISSING!")) or exit(1)
 PROJECT_ID = "1193902598"
 
 session = sa.login_by_id(SESSION_ID, username="superjolt")
@@ -15,19 +15,23 @@ client = cloud.requests()
 
 @client.request
 def get_comment():
-    print("Recieved comment request")
+    print(f"\nRecieved comment request from {client.get_requester()}")
     comments = list(project.comments())
     if comments:
         comment = random.choice(comments)
-        print(f"Selected comment: {comment.content}, by {comment.author_name}")
+        print(f"\nSelected comment: {comment.content}, by {comment.author_name}")
         return [comment.author_name, comment.content]
     else:
         print("No comments found.")
         return ["error", "No comments found! Comment something!"]
-    return ""
 
 @client.event
 def on_ready():
-    print("Request handler is running")
+    print("Request handler is running") 
 
 client.start(thread=True)
+
+if __name__ == "__main__":
+    while True:
+        # your entire script logic goes here
+        pass
